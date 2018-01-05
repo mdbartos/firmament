44c931c0c3978ba808b23ca417e42230aad5e6d3
Matt Bartos <mdbartos@umich.edu>
2018-01-05 03:04:49 -0500

    Disable all components by default

--- cypress/master.cydsn/peripheral_calls.c
+++ cypress/master.cydsn/peripheral_calls.c
@@ -20,16 +20,10 @@ float readings[MAIN_BUFFER_LEN] = {0};
 int array_ix = 0u;
 
 uint8 run_peripherals(){
-    generic_uart_get_reading(mb7383_ttl_0);
-    generic_uart_get_reading(gs3_ttl_0);
-    analog_delsig_get_reading(battery_internal_0);
     return 1u;
 }
 
 uint8 zip_peripherals(){
-    zip_measurements(labels, readings, mb7383_ttl_0, &array_ix, MAIN_BUFFER_LEN);
-    zip_measurements(labels, readings, gs3_ttl_0, &array_ix, MAIN_BUFFER_LEN);
-    zip_measurements(labels, readings, battery_internal_0, &array_ix, MAIN_BUFFER_LEN);
     array_ix = 0u;
     return 1u;
 }
